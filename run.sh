56f109bfeca9776ce8aa0bd42c49950308e9d160
Your Name <vemasani.pavani@gmail.com>
2025-09-06 23:59:48 +0530

    expense

--- run.sh
+++ run.sh
@@ -7,36 +7,36 @@ git clone https://github.com/pdevops78/${project_name}-${component}
 cd ${project_name}-${component}
 echo ${project_name}-${component}
 
-if [ "${db_name}" == 'mysql' ]; then
-    if [ -z "${DB_HOST}" ]; then
-      echo DB_HOST is missing
-      exit 1
-    fi
-
-    if [ -z "${NEW_RELIC_APP_NAME}" ]; then
-      echo NEW_RELIC_APP_NAME is missing
-      exit 1
-    fi
-
-    if [ -z  "${NEW_RELIC_LICENSE_KEY}" ]; then
-        echo NEW_RELIC_LICENSE_KEY is missing
-        exit 1
-    fi
-
-    if [ -z  "${project_name}" ]; then
-        echo project_name is missing
-        exit 1
-    fi
-
-    if [ -z  "${component}" ]; then
-        echo component is missing
-        exit 1
-    fi
-    mysql -h${DB_HOST} -u${rds_name} -p${rds_password} <schema/${component}.sql
-    echo mysql -h${DB_HOST} -u${rds_name} -p${rds_password} <schema/${component}.sql
-fi
-
-if [ "${db_type}" == 'mongo' ]; then
+#if [ "${db_name}" == 'mysql' ]; then
+#    if [ -z "${DB_HOST}" ]; then
+#      echo DB_HOST is missing
+#      exit 1
+#    fi
+#
+#    if [ -z "${NEW_RELIC_APP_NAME}" ]; then
+#      echo NEW_RELIC_APP_NAME is missing
+#      exit 1
+#    fi
+#
+#    if [ -z  "${NEW_RELIC_LICENSE_KEY}" ]; then
+#        echo NEW_RELIC_LICENSE_KEY is missing
+#        exit 1
+#    fi
+#
+#    if [ -z  "${project_name}" ]; then
+#        echo project_name is missing
+#        exit 1
+#    fi
+#
+#    if [ -z  "${component}" ]; then
+#        echo component is missing
+#        exit 1
+#    fi
+#    mysql -h${DB_HOST} -u${rds_name} -p${rds_password} <schema/${component}.sql
+#    echo mysql -h${DB_HOST} -u${rds_name} -p${rds_password} <schema/${component}.sql
+#fi
+#
+#if [ "${db_type}" == 'mongo' ]; then
   if [ -z "${DOCUMENTDB}" ]; then
       echo "Environment variable is missing"
       exit 1;
@@ -47,7 +47,7 @@ if [ "${db_type}" == 'mongo' ]; then
   fi
   echo mongosh my-docdb.cluster-cvkemucwmc9p.us-east-1.docdb.amazonaws.com:27017 --tls --tlsCAFile /app/rds-combined-ca-bundle.pem --username docdb --password roboshop123
   mongosh my-docdb.cluster-cvkemucwmc9p.us-east-1.docdb.amazonaws.com:27017 --tls --tlsCAFile /app/rds-combined-ca-bundle.pem --username docdb --password roboshop123 </app/schema/catalogue.js
-fi
+
 
 
 
