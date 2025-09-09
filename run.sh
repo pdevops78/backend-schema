ce176493b5add290ed9643ae17583c070ff2ce19
Your Name <vemasani.pavani@gmail.com>
2025-09-09 16:06:26 +0530

    backend

--- run.sh
+++ run.sh
@@ -1,30 +1,33 @@
 source /data/secrets.txt
 cat  /data/secrets.txt
-echo
-env
 
 if [ -z "${DB_HOST}" ]; then
   echo Environment variable is missing
+  echo $DB_HOST
   exit 1
 fi
 
-if [ -z "${rds_name}" ]; then
+if [ -z "${rds_username}" ]; then
   echo Environment variable is missing
+  echo rds_username
   exit 1
 fi
 
 if [ -z "${rds_password}" ]; then
   echo Environment variable is missing
+  echo $rds_password
   exit 1
 fi
 
 if [ -z "${project_name}" ]; then
   echo Environment variable is missing
+  echo $project_name
   exit 1
 fi
 
 if [ -z "${component}" ]; then
    echo Environment variable is missing
+   echo $component
    exit 1
 fi
 
@@ -33,7 +36,9 @@ cd ${project_name}-${component}
 echo ${project_name}-${component}
 echo mysql -hmysql-dev.cvkemucwmc9p.us-east-1.rds.amazonaws.com -uadmin -pExpenseApp123 <schema/backend.sql
 
- mysql -hmysql-dev.cvkemucwmc9p.us-east-1.rds.amazonaws.com -uadmin -pExpenseApp123 <schema/backend.sql
+echo mysql -h${DB_HOST} -u${rds_username} -p${rds_password}  <schema/backend.sql
+
+mysql -hmysql-dev.cvkemucwmc9p.us-east-1.rds.amazonaws.com -uadmin -pExpenseApp123 <schema/backend.sql
 
 
 
