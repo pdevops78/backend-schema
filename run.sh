b1f2da3a9959ca138c0b0a818cb8a7f65529fa73
Your Name <vemasani.pavani@gmail.com>
2025-09-09 10:17:32 +0530

    backend

--- run.sh
+++ run.sh
@@ -3,13 +3,36 @@ cat  /data/secrets.txt
 echo
 env
 
+if [ -z "${DB_HOST}" ]; then
+  echo Environment variable is missing
+  exit 1
+fi
 
+if [ -z "${rds_name}" ]; then
+  echo Environment variable is missing
+  exit 1
+fi
+
+if [ -z "${rds_password}" ]; then
+  echo Environment variable is missing
+  exit 1
+fi
+
+if [ -z "${project_name}" ]; then
+  echo Environment variable is missing
+  exit 1
+fi
+
+if [ -z "${component}" ]; then
+   echo Environment variable is missing
+   exit 1
+fi
 
 git clone https://github.com/pdevops78/${project_name}-${component}
 cd ${project_name}-${component}
 echo ${project_name}-${component}
-
-mysql -h${DB_HOST} -u${RDS_DB_USER} -p${RDS_DB_PASSWORD} <schema/${component}.sql
+echo mysql -h${DB_HOST} -u${rds_name} -p${rds_password} <schema/${component}.sql
+mysql -h${DB_HOST} -u${rds_name} -p${rds_password} <schema/${component}.sql
 
 
 
